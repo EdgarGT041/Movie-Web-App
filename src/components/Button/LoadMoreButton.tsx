70a885a2cf1e3b74e4162fb1d9f8292e930463c1
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-01-30 22:59:22 -0700

    feat: add LoadMoreButton component and implement pagination for top rated movies

---
+++ src/components/Button/LoadMoreButton.tsx
@@ -0,0 +1,10 @@
+
+function LoadMoreButton() {
+  return (
+    <div className='flex justify-center mt-2'>
+      <h1 className='text-xl font-medium border w-fit px-6 py-2 cursor-pointer hover:text-black hover:bg-yellow-500'>Load More...</h1>
+    </div>
+  )
+}
+
+export default LoadMoreButton
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,27 +3,38 @@ import HomeSlider from '../../components/Home/HomeSlider'
 import { baseApi } from '../../api/axiosInstance'
 import type { MovieCardType } from '../../utils/constant'
 import MovieList from '../../components/Home/MovieList'
+import LoadMoreButton from '../../components/Button/LoadMoreButton'
 function Home() {
 const [movies, setmovies] = useState<MovieCardType[]>([])
+const [page, setPage] = useState<number>(1)
 
 useEffect(() => {
-  const fetchMovies = async () => {
+  const fetchMovies = async (page: number) => {
     try {
-      const response = await baseApi.get(`/3/movie/top_rated?language=en-US&page=1`)
+      const response = await baseApi.get(`/3/movie/top_rated?language=en-US&page=${page}`)
       // console.log(response.data.results)
-      setmovies(response.data.results)
+      setmovies(prev => [...prev, ...response.data.results])
     } catch (err) {
       console.log("Fetch Error in homepage Top rated movies", err)
     }
   }
   
-  fetchMovies()
-}, [])
+  fetchMovies(page)
+  console.log('render')
+}, [page])
+
+const handlePageUpdate = () => {
+  setPage(prev => prev + 1)
+}
 
   return (
-    <div className='w-[90%] mx-auto'>
+    <div className='w-[90%] mx-auto mb-44'>
       <HomeSlider></HomeSlider>
       <MovieList movies={movies}></MovieList>
+      <div onClick={handlePageUpdate}>
+      <LoadMoreButton></LoadMoreButton>
+
+      </div>
       </div>
   )
 }
