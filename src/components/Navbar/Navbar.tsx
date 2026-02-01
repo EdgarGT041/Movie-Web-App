9a0e2fcc5189cf8892649b269ffd635077b80ffc
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-02-01 16:10:43 -0700

    fix: correct JSX structure in Navbar component links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,21 @@
-
+import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav className="bg-[#121212] py-2">
         <div className="items-center justify-between flex w-[80%] mx-auto">
             <div className="flex space-x-16">
+                <Link to="/">
                 <div className="flex flex-col text-yellow-500">
                     <h1 className="text-[18px] leading-4">ALLABOUT</h1>
                     <h1 className="text-[24px] leading-5 font-semibold">MOVIES</h1>
 
                 </div>
+                </Link>
+                <Link to="/movies">
                 <button className="text-yellow-500 text-[18px]  hover:underline ">
                     EXPLORE
                 </button>
+                </Link>
             </div>
             <div className=" ">
                 <input 
