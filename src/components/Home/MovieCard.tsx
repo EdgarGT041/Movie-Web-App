be7bb60ceed7ef80e18789e92da9a52122e63c3a
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-02-02 17:16:07 -0700

    feat: enhance MovieCard and Details components with improved layout and data fetching

--- src/components/Home/MovieCard.tsx
+++ src/components/Home/MovieCard.tsx
@@ -1,36 +1,47 @@
-import { imagepath, type MovieCardType } from '../../utils/constant'
-import { useState } from 'react'
+import { imagepath, type MovieCardType } from "../../utils/constant";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 interface MovieCardProps {
-    movieData: MovieCardType
+  movieData: MovieCardType;
 }
 
 function MovieCard({ movieData }: MovieCardProps) {
-    const [hover, setHover] = useState<null | number>(null)
+  const [hover, setHover] = useState<null | number>(null);
   return (
-                    <div className='col'
-                    
-                        onMouseEnter={() => setHover(movieData.id)}
-                        onMouseLeave={() => setHover(null)}  >
-
-                    <div className={`my-3 border-2 border-zinc-800 rounded-lg overflow-hidden ${movieData.id === hover ? "scale-[102%] text-yellow-500" : ""} duration-200 transition-transform`}>
-                        {/* "card m-2 bg-zinc-900 hover:scale-105 transition-transform duration-300" */}
-                        <div className="relative overflow-hidden">
-                        <img src={imagepath + movieData.poster_path} alt="" className='aspect-[3/4]' />
-                        <div className="absolute -bottom-6 w-full h-28 _carouselGradient"></div>
-                        </div>
-                        <div className="bg-[#222] p-2 ">
-                        <h1 className='text-[17px] font-semibold line-clamp-1'>{movieData.title}</h1>
-                        <div className='text-zinc-300 text-[15px] mt-2'>
-
-                            <h1 className='text-[15px] text-zinc-300'>Rating: {String(movieData.vote_average).substring(0, 3)}</h1>
-                            <h1 className=''>Language: {movieData.original_language}</h1>
-                            <h1 className=''>Release: {movieData.release_date}</h1>
-
-                        </div>
-                        </div>
-                    </div>
-
-                    </div>  )
+    <Link to={'/details/' + movieData.id}>
+      <div
+        className="col"
+        onMouseEnter={() => setHover(movieData.id)}
+        onMouseLeave={() => setHover(null)}
+      >
+        <div
+          className={`my-3 border-2 border-zinc-800 rounded-lg overflow-hidden ${movieData.id === hover ? "scale-[102%] text-yellow-500" : ""} duration-200 transition-transform`}
+        >
+          {/* "card m-2 bg-zinc-900 hover:scale-105 transition-transform duration-300" */}
+          <div className="relative overflow-hidden">
+            <img
+              src={imagepath + movieData.poster_path}
+              alt=""
+              className="aspect-[3/4]"
+            />
+            <div className="absolute -bottom-6 w-full h-28 _carouselGradient"></div>
+          </div>
+          <div className="bg-[#222] p-2 ">
+            <h1 className="text-[17px] font-semibold line-clamp-1">
+              {movieData.title}
+            </h1>
+            <div className="text-zinc-300 text-[15px] mt-2">
+              <h1 className="text-[15px] text-zinc-300">
+                Rating: {String(movieData.vote_average).substring(0, 3)}
+              </h1>
+              <h1 className="">Language: {movieData.original_language}</h1>
+              <h1 className="">Release: {movieData.release_date}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { baseApi } from "../../api/axiosInstance";
+import { type MovieDetailType } from "../../utils/constant";
+import { imagepath } from "../../utils/constant";
 function Details() {
+  const params = useParams();
+  console.log(params);
+  const [details, setDetails] = useState<MovieDetailType>();
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const response = await baseApi.get(
+          `/3/movie/${params.id}?language=en-US`,
+        );
+        setDetails(response.data);
+      } catch (error) {
+        console.log("Fetch Details error", error);
+      }
+    };
+
+    fetchDetails();
+  }, [params]);
   return (
-    <div>Details</div>
-  )
+    <div>
+      {details && (
+        <div className="relative h-fit w-full ">
+          <div className="relative ">
+            <img
+              src={imagepath + details.backdrop_path}
+              className="opacity-40 w-full aspect-[7/4] object-center"
+              alt="background"
+            />
+            <div className="absolute bottom-0 h-full w-full _carouselGradient"></div>
+          </div>
+          <div className="absolute top-0 pb-[100px] w-full">
+            <div className="mt-[500px] w-[90%] mx-auto">
+              <div className="flex gap-8">
+                <img
+                  src={imagepath + details.poster_path}
+                  className="w-[350px] h-fit"
+                  alt=""
+                />
+                <div className="">
+                  <h1 className="text-5xl font-semibold">
+                    {details?.original_title}
+                    <span className="mx-3 text-4xl">
+                      ({details?.release_date?.substring(0, 4)})
+                    </span>
+                  </h1>
+                  <div className="text-xl text-slate-300 mt-2">
+                    <h2>{details?.tagline}</h2>
+                    <h2 className="mt-2">{details?.overview}</h2>
+                    <div className="flex flex-col gap-3 mt-4 text-zinc-300">
+                      <h2>
+                        Genres:{" "}
+                        {details?.genres?.map((genre) => genre.name).join(", ")}
+                      </h2>
+                      <h2>
+                        Rating: {String(details?.vote_average).substring(0, 3)}
+                      </h2>
+                      <h2>Original Language: {details?.original_language}</h2>
+                      <h2>Release Date: {details?.release_date}</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Details
+export default Details;
