2843830fdfd748820cd682649eb23b326e9c239f
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-02-03 16:26:46 -0700

    feat: update HomeCarousel and HomeCarouselList components to use Link for navigation

--- src/components/Home/HomeCarousel.tsx
+++ src/components/Home/HomeCarousel.tsx
@@ -1,48 +1,58 @@
-import { FiThumbsUp } from 'react-icons/fi'
-import { imagepath, type CarouselMovieType } from '../../utils/constant'
+import { FiThumbsUp } from "react-icons/fi";
+import { imagepath, type CarouselMovieType } from "../../utils/constant";
+import { Link } from "react-router-dom";
 
 interface HomeCarouselProps {
-    carouselMovies: CarouselMovieType[];
+  carouselMovies: CarouselMovieType[];
 }
 
 function HomeCarousel({ carouselMovies }: HomeCarouselProps) {
   return (
-      <div className="carousel-inner">
-          {carouselMovies.map((movie, index) => (
-            <div key={index}  className={`carousel-item ${index == 0 ? "active" : ""}`}>
-              <div className="relative">
-                {/* <div className="block bg-red-300 aspect-[7/4]"></div> */}
-                <img
-                  src={imagepath + movie.backdrop_path}
-                  className="w-full aspect-[7/4]"
-                  alt=""
-                />
+    <div className="carousel-inner">
+      {carouselMovies.map((movie, index) => (
+        <Link to={'/details/' + movie.id} key={index}>
+        <div
+          className={`carousel-item ${index == 0 ? "active" : ""}`}
+        >
+          <div className="relative">
+            {/* <div className="block bg-red-300 aspect-[7/4]"></div> */}
+            <img
+              src={imagepath + movie.backdrop_path}
+              className="w-full aspect-[7/4]"
+              alt=""
+              />
 
-                <div className={'w-full absolute h-full top-0 left-0 bg-black opacity-[0.1] hover:opacity-[0.3] transition-opacity duration-200'}></div>
+            <div
+              className={
+                "w-full absolute h-full top-0 left-0 bg-black opacity-[0.1] hover:opacity-[0.3] transition-opacity duration-200"
+              }
+            ></div>
 
-                <div className="absolute bottom-0 h-44 w-full _carouselGradient"></div>
-              </div>
-              <div className="absolute bottom-0 flex items-end gap-4 px-4">
-                {/* <div className="block bg-blue-400 w-[160px] aspect-[4/5]"></div> */}
-                <img
-                  src={imagepath + movie.poster_path}
-                  className="w-[160px] aspect-[4/5]"
-                  alt=""
-                />
-                <div className="flex flex-col gap-1">
-                  <h1 className="text-4xl text-white">{movie.title}</h1>
-                  <h2 className="w-[500px] text-xl line-clamp-3 text-zinc-400">
-                    {movie.overview}
-                  </h2>
-                  <div className="flex items-center gap-1 text-zinc-400 text-xl">
-                    <FiThumbsUp />
-                    <h3 className="text-lg">{movie.vote_count}</h3>
-                  </div>
-                </div>
+            <div className="absolute bottom-0 h-44 w-full _carouselGradient"></div>
+          </div>
+          <div className="absolute bottom-0 flex items-end gap-4 px-4">
+            {/* <div className="block bg-blue-400 w-[160px] aspect-[4/5]"></div> */}
+            <img
+              src={imagepath + movie.poster_path}
+              className="w-[160px] aspect-[4/5]"
+              alt=""
+            />
+            <div className="flex flex-col gap-1">
+              <h1 className="text-4xl text-white">{movie.title}</h1>
+              <h2 className="w-[500px] text-xl line-clamp-3 text-zinc-400">
+                {movie.overview}
+              </h2>
+              <div className="flex items-center gap-1 text-zinc-400 text-xl">
+                <FiThumbsUp />
+                <h3 className="text-lg">{movie.vote_count}</h3>
               </div>
             </div>
-          ))}
-        </div>  )
+          </div>
+        </div>
+      </Link>
+      ))}
+    </div>
+  );
 }
 
-export default HomeCarousel
+export default HomeCarousel;
--- src/components/Home/HomeCarouselList.tsx
+++ src/components/Home/HomeCarouselList.tsx
@@ -1,42 +1,53 @@
-import { useState } from 'react';
-import type { CarouselMovieType } from '../../utils/constant';
-import { imagepath } from '../../utils/constant';
-import { FiThumbsUp } from 'react-icons/fi';
+import { useState } from "react";
+import type { CarouselMovieType } from "../../utils/constant";
+import { imagepath } from "../../utils/constant";
+import { FiThumbsUp } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 interface HomeCarouselListProps {
-    next: number[];
-    carouselMovies: CarouselMovieType[];
+  next: number[];
+  carouselMovies: CarouselMovieType[];
 }
 
 function HomeCarouselList({ next, carouselMovies }: HomeCarouselListProps) {
-    const[hover,setHover]=useState<null|number>(null);
+  const [hover, setHover] = useState<null | number>(null);
 
   return (
     <div>
-        <h1 className='font-bold, text-xl text-yellow-500'>Upcoming Movies</h1>
-        {
-            next.map((item,index)=>(
-                <div  key={index} className='flex gap-2'
-                onMouseEnter={() => setHover(index)}
-                onMouseLeave={() => setHover(null)}
-                >
-                    <img src={imagepath+carouselMovies[item]?.poster_path} alt='' className='w-[100px]'/>
-                    <div className='flex flex-col justify-between ml-2 py-2'>
-
-                    <div className='leading-5'>
-                        <h1 className={`${hover === index ? 'text-yellow-500' : ''}`} >{carouselMovies[item]?.title}</h1>
-                        <h1 className='text-zinc-300 text-md line-clamp-3'>{carouselMovies[item]?.overview}</h1>
-                    </div>
-                    <div className='gap-1 flex items-center text-center'> <FiThumbsUp/>
-                        <h2>{carouselMovies[item]?.vote_count}</h2>
-                    </div>
-
-                    </div>
-                </div>
-            ))
-        }
+      <h1 className="font-bold, text-xl text-yellow-500">Upcoming Movies</h1>
+      {next.map((item, index) => (
+        <Link to={"/details/" + carouselMovies[item]?.id}>
+          <div
+            key={index}
+            className="flex gap-2"
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(null)}
+          >
+            <img
+              src={imagepath + carouselMovies[item]?.poster_path}
+              alt=""
+              className="w-[100px]"
+            />
+            <div className="flex flex-col justify-between ml-2 py-2">
+              <div className="leading-5">
+                <h1 className={`${hover === index ? "text-yellow-500" : ""}`}>
+                  {carouselMovies[item]?.title}
+                </h1>
+                <h1 className="text-zinc-300 text-md line-clamp-3">
+                  {carouselMovies[item]?.overview}
+                </h1>
+              </div>
+              <div className="gap-1 flex items-center text-center">
+                {" "}
+                <FiThumbsUp />
+                <h2>{carouselMovies[item]?.vote_count}</h2>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
- 
-export default HomeCarouselList
+
+export default HomeCarouselList;
