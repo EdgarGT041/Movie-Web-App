f458eafd66a1b794765c4d72744e5fc8435d9c43
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-02-07 13:46:14 -0700

    refactor:  move API credentials to environment variables

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const baseApi=axios.create({
-    baseURL: "https://api.themoviedb.org",
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjBhMTg3OTNmN2QxZjE3ODQ0ZWE4OWJkNTdhYzk5YiIsIm5iZiI6MTc2OTQ3OTk1Mi41MTgwMDAxLCJzdWIiOiI2OTc4MWYxMGRkZjBlYmUxYTQ2OTUyNDAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.w4vYQALYLhVi1EBH1LYJJ0N6gmbsARtju5NBVGum5I0'
-                    }
+export const baseApi = axios.create({
+    baseURL: import.meta.env.VITE_TMDB_BASE_URL,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`
+    }
 })
